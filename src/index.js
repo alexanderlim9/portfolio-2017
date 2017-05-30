1a2a097062e3319393267be49ca5af9eaa2c5c10
ALEX LIM <alim@ccs.neu.edu>
2017-05-29 22:48:48 -0400

    create splash component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Nav from './nav';
+import Splash from './splash';
+
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<Nav />, document.getElementById('root'));
+ReactDOM.render(<Nav />, document.getElementById('nav'));
+ReactDOM.render(<Splash />, document.getElementById('splash'));
+
 registerServiceWorker();
+
---
+++ src/splash.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class Splash extends Component {
+  render() {
+    return (
+      <div className="container splash">
+        <div className="row">
+          <div className="col-xs-4 col-xs-offset-3">
+            <p>
+              Lorem ipsum dolor sit amet consectetur
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Splash;
