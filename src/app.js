dc92c03167e8742a1495ec961d40c99b085391d8
ALEX LIM <alim@ccs.neu.edu>
2017-08-31 18:22:13 -0400

    PIVOT

--- src/app.js
+++ src/app.js
@@ -1,23 +1,12 @@
 import React, {Component} from 'react';
 
-import Nav from './nav';
-import Splash from './splash';
-import Work from './sections/work';
-import About from './sections/about';
-import Resume from './sections/resume';
-import Contact from './sections/contact';
-
+import Greeting from './greeting';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Nav />
-        <Splash />
-        <Work />
-        <About />
-        <Resume />
-        <Contact />
+        <Greeting />
       </div>
     );
   }
---
+++ src/greeting.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class Greeting extends Component {
+  render() {
+    return (
+      <div className="container greeting">
+        <div className="row">
+          <div className="col-xs-12">
+            <h1>
+              Hello!
+            </h1>
+            <p>My name is Alexander Lim, but you can call me Alex.</p>
+            {/*<p>There's a lot of data floating around and I want to make sense of it all.</p>*/}
+            {/*<p>I am a:*/}
+            {/*<ul>*/}
+            {/*<li>Developer</li>*/}
+            {/*<li>Designer</li>*/}
+            {/*<li></li>*/}
+            {/*</ul>*/}
+            {/*</p>*/}
+          </div>
+          <div className="row">
+            <div className="col-xs-4 col-xs-offset-3">
+              <label>Let me tell you all about...</label>
+            </div>
+            <div className="col-xs-5">
+              <button>myself</button>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-xs-5 col-xs-offset-7">
+              <button>what I'm passionate about</button>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-xs-5 col-xs-offset-7">
+              <button>projects I've done so far</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Greeting;
