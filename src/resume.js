4f7cfdd3ddc516e3035b1d27313e45fe5d5f3463
ALEX LIM <alim@ccs.neu.edu>
2017-10-10 13:50:33 -0400

    update resume, remove resume page copy

--- src/resume.js
+++ src/resume.js
@@ -8,17 +8,17 @@ class Resume extends Component {
   render() {
     return (
       <div className="container about">
-        <div className="row">
+{/*        <div className="row">
           <div className="col-xs-12">
             <h1>
               This is my resume
             </h1>
             <p>I also make great puns.</p>
           </div>
-        </div>
+        </div>*/}
         <div className="row">
           <div className="col-xs-12">
-            <img id="resume-png" src={require("./resume-10-5-17.png")} alt="resume"/>
+            <img id="resume-png" src={require("./resume-10-8-17.png")} alt="resume"/>
           </div>
         </div>
       </div>
