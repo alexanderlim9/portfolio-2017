613e07e1349d2c4b581c644085c6dee6978aae40
ALEX LIM <alim@ccs.neu.edu>
2017-10-11 16:17:07 -0400

    update about page, add photo

--- src/about.js
+++ src/about.js
@@ -6,35 +6,27 @@ class About extends Component {
     return (
       <div className="container about">
         <div className="row">
-          <div className="col-xs-12">
+          <div className="col-xs-4">
+            <img id="headshot" src={require("./headshot.jpg")} alt="headshot"/>
+          </div>
+          <div className="col-xs-8 bio">
             <h1>
-              I'm a lot of things
+              About me
             </h1>
-            <p>and they each define who I am. </p>
-          </div>
-        </div>
-        <div className="row bio">
-          <div className="col-xs-4">
-            <p>I'm a <u>student</u>,</p>
-            <p className="copy">I study Computer Science & Interaction Design at Northeastern
-              University.</p>
-          </div>
-          <div className="col-xs-4">
-            <p>I'm a <u>developer</u>,</p>
-            <p className="copy">I like solving problems through technology.</p>
-          </div>
-          <div className="col-xs-4">
-            <p>I'm a <u>designer</u>,</p>
-            <p className="copy">I value the process required to reach solutions.</p>
-          </div>
-        </div>
-        <div className="row bio-section">
-          <div className="col-xs-12">
-            {/*<p>*/}
-              {/*But I can't separate any one part.*/}
-              {/*<br/>*/}
-              {/*I write code like a designer and I design from a developer's perspective.*/}
-            {/*</p>*/}
+            <p className="copy">
+              My name is Alex, I'm a developer/designer studying Computer Science & Interaction
+              Design at Northeastern University.
+            </p>
+            <p className="copy">
+              I'm passionate about making information accessible and currently stepping into the
+              field of data science. There's so much data out there, it's important not only to make
+              sense of it, but also to communicate it as well.
+            </p>
+            <p className="copy">My multidisciplinary background in computer science, data science,
+              and design allows me to approach problems differently. Empathy is design is one of my
+              greatest values. I believe that building anything without considering your user is
+              effort gone to waste.
+            </p>
           </div>
         </div>
       </div>
