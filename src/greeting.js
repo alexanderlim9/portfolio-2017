8e4995d5b7e6d9247e1ab707a559da3bfa68a142
ALEX LIM <alim@ccs.neu.edu>
2017-10-12 01:20:35 -0400

    update favicon, add link to old site

--- src/greeting.js
+++ src/greeting.js
@@ -23,7 +23,7 @@ class Greeting extends Component {
           </div>
           <div className="row">
             <div className="col-xs-5 col-xs-offset-7">
-              <button>the work I've done</button>
+              <button><a target="_blank" href="https://alexmlim7.myportfolio.com/">the work I've done</a></button>
             </div>
           </div>
           <div className="row">
