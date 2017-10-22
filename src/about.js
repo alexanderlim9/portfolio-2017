d4059bad55bc14ac72957c731cce717424134aa1
ALEX LIM <alim@ccs.neu.edu>
2017-10-22 14:15:27 -0400

    remove new about section

--- src/about.js
+++ src/about.js
@@ -10,7 +10,7 @@ class About extends Component {
     var i = 2;
     var int = setInterval(function () {
       window.scrollTo(0, i);
-      i += (800 - i)/100 + .5;
+      i += (800 - i) / 100 + .5;
       if (i > 800) clearInterval(int);
     }, 1);
   }
@@ -42,6 +42,22 @@ class About extends Component {
             </p>
           </div>
         </div>
+        {/*<div className="row">*/}
+          {/*<div className="col-xs-8 bio">*/}
+            {/*<div id="bio-personal">*/}
+              {/*<p className="copy">There are many things that define me as an individual. I'm proud*/}
+                {/*of my identity as both an Asian-American and as a member of the LGBTQ+*/}
+                {/*community. As such, I value diversity and inclusivity in the workplace and beyond.*/}
+              {/*</p>*/}
+              {/*<p className="copy">*/}
+                {/*When I'm not sitting behind a screen, you can find me running around Boston*/}
+                {/*Ask me how much I love trains, typography, or&nbsp;*/}
+                {/*<a target="_blank" id="tesselate" href="https://soundcloud.com/alt-j/03-tessellate">*/}
+                  {/*Tessellate</a> by Alt-j.*/}
+              {/*</p>*/}
+            {/*</div>*/}
+          {/*</div>*/}
+        {/*</div>*/}
       </div>
     );
   }
